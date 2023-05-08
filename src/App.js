031bfb4fec86a32f486cc4b519dba17cec7ec91c
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-05-08 17:32:55 -0400

    basic front page with contact info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App box">
+      <h1>Dirk Stahlecker Racing</h1>
+      <h2>Micro sprint racer from Cambridge, MA</h2>
+      <a href="mailto:dirkstahleckerracing@gmail.com">dirkstahleckerracing@gmail.com</a>
     </div>
   );
 }
