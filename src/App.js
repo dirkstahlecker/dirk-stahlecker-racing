4eaee5113e70f4553ac016c042a61781b994e6be
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-09-24 01:20:28 -0400

    added photo and driver/crew chiefe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App box">
       <h1>Dirk Stahlecker Racing</h1>
       <h2>#47 Micro Sprint Racer from Cambridge, MA</h2>
+      <h3>
+        Driver: Dirk Stahlecker
+        <br/>
+        Crew Chief: Kip Stahlecker
+      </h3>
+      <img src="main_photo.jpg" width="400" height="300"/>
+      <br/>
+      <br/>
       <a href="mailto:dirkstahleckerracing@gmail.com">dirkstahleckerracing@gmail.com</a>
       <br/>
       <br/>
